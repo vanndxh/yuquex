b4789d4784a9c59c5e95453c7e9ca1170513e302
vanndxh <1025196468@qq.com>
2022-01-11 18:59:44 +0800

    init

---
+++ src/components/common/catgif.vue
@@ -0,0 +1,39 @@
+<template>
+  <div align="left">
+    <el-row>
+      <el-col :span="1">
+        <el-affix position="bottom" :offset="20">
+          <el-popover
+              placement="top-start"
+              :width="200"
+              trigger="click"
+              content="别摸我了，给小黑打个赏吧~"
+              :auto-close="2500"
+          >
+            <template #reference>
+              <img src="../../assets/cat.gif" alt="catgif" style="width: 150px; height: 150px; outline: none">
+            </template>
+          </el-popover>
+        </el-affix>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "catgif",
+  data() {
+    return {
+      fix: 1
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    <n-button>Default</n-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+
+
+</script>
+
+<style>
+
+</style>
